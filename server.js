23b60c941b8cf6d26700321162b0f72c09fb631a
Ayesh <ayeshgeelaka@gmail.com>
2023-01-28 20:55:02 +0530

    Final upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+server.listen(5000);
+
+app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection", function(socket){   
+	socket.on("newuser",function(clientname){
+		socket.broadcast.emit("update", clientname + " joined the conversation");
+	});
+	socket.on("exituser",function(clientname){
+		socket.broadcast.emit("update", clientname + " left the conversation");
+	});
+	socket.on("chat",function(message){
+		socket.broadcast.emit("chat", message);
+
+		
+	});
+});
+
