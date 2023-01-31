a9419ef27975e7a77ce9bac100906381201cb47f
Ayesh <ayeshgeelaka@gmail.com>
2023-01-31 12:34:45 +0530

    New

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 var User = require("./models/user")
-var path = require("path")
 var mongoose = require("mongoose")
 var params = require("./params/params");
 var bodyParser = require("body-parser");
@@ -20,14 +19,6 @@ var flash = require("connect-flash");
 server.listen(5000);
 
 app.use(express.static(path.join(__dirname+"/public")));
-
-mongoose.set('strictQuery', false)
-
-var db = mongoose.connect(params.DATABASECONNECTION, function (error, response) {
-    if (error) { console.log(error); }
-    else { console.log('connected' + db) }
-});
-setUpPassport();
 
 io.on("connection", function(socket){   
 	socket.on("newuser",function(clientname){
@@ -42,42 +33,6 @@ io.on("connection", function(socket){
 		
 	});
 });
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-/* User Login */
-app.post("/login", passport.authenticate("user", {
-  successRedirect: "/public/index.html",
-  failureRedirect: "/login.html",
-  failureFlash: true
-}));
-
-app.post("/signup", function (req, res, next) {
-
-  User.findOne({ email: req.body.email }, function (err, user) {
-      if (err) { return next(err); }
-      if (user) {
-          //req.flash("error", "There's already an account with this email");
-          //return res.redirect("/login");
-          console.log("There's already an account with this email")
-      }
-      var newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          contact: req.body.contact,
-          password: req.body.password
-      });
-      newUser.save(next);
-      res.sendFile(__dirname + '/public/login.html');
-  });
-})
-
-app.get("/logout", function (req, res) {
-	req.logout(function (err) {
-		if (err) { return next(err); }
-		res.sendFile(__dirname + '/public/index.html');
-      });
-  });
 
 
 
